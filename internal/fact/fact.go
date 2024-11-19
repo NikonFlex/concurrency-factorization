c43e194efd35826e6f0195ba955e06a9020afb51
Nikon <nikon.ser.par@gmail.com>
2024-11-20 02:05:37 +0300

    remove check for error

--- internal/fact/fact.go
+++ internal/fact/fact.go
@@ -75,7 +75,7 @@ func (f *factorizationImpl) Do(
 		return err
 	}
 
-	errorCh := make(chan error, checkedConfig.FactorizationWorkers+checkedConfig.WriteWorkers) // Channel to capture errors.
+	errorCh := make(chan error, checkedConfig.WriteWorkers) // Channel to capture errors.
 	context := executionContext{done, errorCh, writer, checkedConfig}
 
 	// Generate channels for processing.
@@ -177,8 +177,6 @@ func startWriting(wg *sync.WaitGroup, resultsCh chan *factorizedNumber, context 
 			for {
 				select {
 				case <-context.done:
-					return
-				case <-context.errorCh:
 					return
 				case result, ok := <-resultsCh:
 					if !ok {
